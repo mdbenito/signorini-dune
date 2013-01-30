1c7f5eb5efab82c7262d27d6abfc1b8c4deca4f7
Miguel de Benito Delgado <nonick@8027.org>
2013-01-30 16:41:38 +0100

    Further progress in [HW05]

--- signorini/activeinactivemapper.hpp
+++ signorini/activeinactivemapper.hpp
@@ -10,41 +10,32 @@
 
 /*! A mapper to order entities according to their status as active or inactive.
  
+ The constructor is given three sets of entities representing those which are
+ active "A", inactive "I" and the rest "N". The final ordering returned by the
+ mapper will be:
  
- Using the specified functor "gap" the entities are divided in two sets 
- corresponding to those which belong to the support (set "S") and those who
- don't (set "N")
- 
- The set "S" is further divided in active "A" and inactive "I", according to
- an additional functor "contact"
- 
- FIXME!!!!!!!!!!!!!!!!!!!!!
- We use a dirty and very ugly hack which relies on Mapper's bad interface: the
- indices returned by Mapper are platform dependent *signed* ints, so we can use
- the sign bit to store whether a particular index is for an entity in the support
- or not.
- 
- This leaves enough room on my 64 bit machine and compiler, but looks like very
- bad practice indeed. However it's fast and easy.
+    N_1 ... N_r I_1 ... I_m A_1 ... A_n 
  
  Template parameters:
  TGV: TGridView
- TFN: TFunctor   (for the gap function)
- TAC: TActiveFunctor (to determine active entities in the 
  */
-template <int codim, class TGV, class TFN, class TAC>
+template <int codim, class TGV>
 class ActiveInactiveMapper
-: public Mapper<typename TGV::Grid, ActiveInactiveMapper<codim, TGV, TFN, TAC> >
+: public Mapper<typename TGV::Grid, ActiveInactiveMapper<codim, TGV> >
 {
-  typedef typename TGV::template Codim<0>::Entity Entity;
+  typedef typename TGV::template Codim<codim>::Entity Entity;
+  typedef typename TGV::EntityPointer EntityPointer;
+  typedef std::vector<EntityPointer> EntityPointerVector;
   
-  const TFN&     gap;
-  const TAC& contact;
   const TGV&      gv;
   const typename TGV::IndexSet& iset;
-  
+  int*     indices;
+
 public:
-  ActiveInactiveMapper (const TGV& _gv, const TFN& _gap, const TAC& _contact);
+  ActiveInactiveMapper (const TGV& _gv,
+                        const EntityPointerVector& active,
+                        const EntityPointerVector& inactive,
+                        const EntityPointerVector& others);
   
   template<class EntityType> int map (const EntityType& e) const;
   int map (const Entity& e, int i, unsigned int cc) const;
@@ -54,99 +45,108 @@ public:
   bool contains (const EntityType& e, int& result) const;
   
   int size () const;
-  void update ();
-  
-private:
-  int gapSupported;  //<! Number of elements in the support of the gap functor
-  int       active;
-  int        total;  //<! Total number of elements
-  int*     indices;
+  void update (const EntityPointerVector& active,
+               const EntityPointerVector& inactive,
+               const EntityPointerVector& others);
 };
 
 
-template <int codim, class TGV, class TFN, class TAC>
-ActiveInactiveMapper<codim, TGV, TFN, TAC>::ActiveInactiveMapper (const TGV& _gv,
-                                                                  const TFN& _gap,
-                                                                  const TAC& _contact)
-: gv (_gv), gap (_gap), contact (_contact), iset (_gv.indexSet()), indices (NULL)
+template <int codim, class TGV>
+ActiveInactiveMapper<codim, TGV>::ActiveInactiveMapper (const TGV& _gv,
+                                                        const EntityPointerVector& active,
+                                                        const EntityPointerVector& inactive,
+                                                        const EntityPointerVector& others)
+: gv (_gv), iset (_gv.indexSet()), indices (NULL)
 {
-  update ();
+  update (active, inactive, others);
 }
 
 
-template <int codim, class TGV, class TFN, class TAC>
+template <int codim, class TGV>
 template<class EntityType>
-int ActiveInactiveMapper<codim, TGV, TFN, TAC>::map (const EntityType& e) const
+int ActiveInactiveMapper<codim, TGV>::map (const EntityType& e) const
 {
-  int idx = indices[iset.index (e)];
-  return (idx < 0) ? (-1*idx + gapSupported-1) : idx;
+    //int idx = indices[iset.index (e)];
+    //return (idx < 0) ? (-1*idx + gapEntities.size()-1) : idx;
+  return indices[iset.index (e)];
 }
 
 
-template <int codim, class TGV, class TFN, class TAC>
-int ActiveInactiveMapper<codim, TGV, TFN, TAC>::map (const Entity& e, int i,
-                                                     unsigned int cc) const
+template <int codim, class TGV>
+int ActiveInactiveMapper<codim, TGV>::map (const Entity& e, int i,
+                                           unsigned int cc) const
 {
-  int idx = indices[iset.index (e, i, cc)];
-  return (idx < 0) ? (-1*idx + gapSupported-1) : idx;
+    //int idx = indices[iset.index (e, i, cc)];
+    //return (idx < 0) ? (-1*idx + gapEntities.size()-1) : idx;
+  indices[iset.index (e, i, cc)];
 }
 
 
-template <int codim, class TGV, class TFN, class TAC>
-bool ActiveInactiveMapper<codim, TGV, TFN, TAC>::contains (const Entity& e, int i,
-                                                           int cc, int& result) const
+template <int codim, class TGV>
+bool ActiveInactiveMapper<codim, TGV>::contains (const Entity& e, int i,
+                                                 int cc, int& result) const
 {
   DUNE_THROW (Exception, "not implemented");
 }
 
 
-template <int codim, class TGV, class TFN, class TAC>
+template <int codim, class TGV>
 template <class EntityType>
-bool ActiveInactiveMapper<codim, TGV, TFN, TAC>::contains (const EntityType& e,
-                                                           int& result) const
+bool ActiveInactiveMapper<codim, TGV>::contains (const EntityType& e,
+                                                 int& result) const
 {
   DUNE_THROW (Exception, "not implemented");
 }
 
 
-template <int codim, class TGV, class TFN, class TAC>
-int ActiveInactiveMapper<codim, TGV, TFN, TAC>::size () const
+template <int codim, class TGV>
+int ActiveInactiveMapper<codim, TGV>::size () const
 {
-  return total;
+  return gv.size (codim);
 }
 
 
-template <int codim, class TGV, class TFN, class TAC>
-void ActiveInactiveMapper<codim, TGV, TFN, TAC>::update ()
+template <int codim, class TGV>
+void ActiveInactiveMapper<codim, TGV>::update (const EntityPointerVector& active,
+                                               const EntityPointerVector& inactive,
+                                               const EntityPointerVector& others)
 {
-  gapSupported = 0;
-
+  delete indices; indices = new int[gv.size (codim)];
+  int cnt = 0;
+  for (auto& x : others)   indices[iset.index(x)] = cnt++;
+  for (auto& x : inactive) indices[iset.index(x)] = cnt++;
+  for (auto& x : active)   indices[iset.index(x)] = cnt++;
+  
+  /*
+  gapEntities.clear();
   total = gv.size (codim);
   delete indices;
   indices = new int[total];
-  int cnt = -1;
-  
-  std::vector<typename TGV::EntityPointer> inGap (total/2);  // z.B...
+  int cnt = 0;
 
   for (auto it = gv.template begin<codim>(); it != gv.template end<codim>(); ++it) {
     if (gap.isSupported (it->geometry())) {
-      inGap << *it;
-      gapSupported++;
+      gapEntities << *it;
     } else {
-      indices[iset.index(*it)] = cnt--;
+      indices[iset.index(*it)] = cnt++;
     }
   }
   
   if (gapSupported == 0)
     DUNE_THROW (Exception, "functor had empty support");
-  active = 0;
-  cnt = 1;
-  for (auto& x : inGap) {
-    if (contact.isSupported(x.geometry()))
-      indices[iset.index(x)] = active++;
-    else
-      indices[iset.index(x)] = gapSupported-(cnt--);
+
+  activeEntities.clear();
+  int active = 0;
+  int cnt2 = -1;
+  for (auto& x : gapEntities) {
+    if (contact.isSupported (x.geometry())) {
+      activeEntities << *it;
+      indices[iset.index(x)] = cnt + gapEntities.size() + cnt2--;
+    } else {
+      indices[iset.index(x)] = cnt + active++;
+    }
   }
+   */
 }
 
 
--- signorini/activeset.hpp
+++ signorini/activeset.hpp
@@ -45,51 +45,67 @@ using std::cout;
     TTF: TTractionsFunctor
     TGF: TGapFunctor
     TSS: TShapeSet
+    TLM: TLagrangeMultipliersShapeSet
  */
-template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS>
+template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS, class TLM>
 class SignoriniIASet
 {
 public:
   static const int dim = TGV::dimension;
   
-  typedef            typename TGV::ctype ctype;
-  typedef      FieldVector<ctype, dim> coord_t;
-  typedef FieldMatrix<ctype, dim, dim> block_t;
-  typedef      BCRSMatrix<block_t> BlockMatrix;
-  typedef     BlockVector<coord_t> CoordVector;
+  typedef typename TGV::EntityPointer        EntityPointer;
+  typedef std::vector<const EntityPointer>   EntityPointerVector;
+
+  typedef typename TGV::ctype                        ctype;
+  typedef FieldVector<ctype, dim>                  coord_t;
+  typedef FieldMatrix<ctype, dim, dim>             block_t;
+  typedef BCRSMatrix<block_t>                  BlockMatrix;
+  typedef BCRSMatrix<coord_t>                 VectorMatrix;
+  typedef BlockVector<coord_t>                 CoordVector;
   typedef BlockVector<FieldVector<ctype, 1> > ScalarVector;
+  typedef ActiveSetFunctor<ctype, dim, ScalarVector>    AIFunctor;
+  typedef ActiveInactiveMapper<dim, TGV> AIMapper;
   typedef LeafMultipleCodimMultipleGeomTypeMapper<typename TGV::Grid,
                                                   MCMGVertexLayout> VertexMapper;
   typedef FunctorSupportMapper<dim, TGV, TGT> GapVertexMapper;
-  typedef ActiveSetFunctor<ctype, dim, ScalarVector> AIFunctor;
-  typedef ActiveInactiveMapper<dim, TGV, TGT, AIFunctor> AIMapper;
 
 private:
-  const TGV& gv;  //!< Grid view
+  const TGV& gv;    //!< Grid view
   
-  const TET& a;   //!< Elasticity tensor
-  const TFT& f;   //!< Volume forces
-  const TTT& p;   //!< Boundary forces
+  const TET& a;     //!< Elasticity tensor
+  const TFT& f;     //!< Volume forces
+  const TTT& p;     //!< Boundary forces
   const TGT& gap;   //!< Normal gap function (scalar)
   
-  block_t      I;  //!< Identity matrix block (Dune::DiagonalMatrix not working?)
-  BlockMatrix  A;  //!< Stiffness matrix
-  BlockMatrix  D;  //!< See [HW04, eq. (3.5)]
-  CoordVector  b;  //!< RHS: volume forces and tractions
-  CoordVector  u;  //!< Solution
-  ScalarVector g;  //!< Gap functor evaluated at the gap nodes
+  block_t      I;   //!< Identity matrix block (Dune::DiagonalMatrix not working?)
+  BlockMatrix  A;   //!< Stiffness matrix
+  BlockMatrix  D;   //!< See [HW04, eq. (3.5)]
+  VectorMatrix N;   //!< See [HW04, eq. (3.5)]
+  VectorMatrix T;   //!< See [HW04, eq. (3.5)]
+  CoordVector  b;   //!< RHS: volume forces and tractions
+  CoordVector  u;   //!< Solution
+  CoordVector  m;   //!< Lagrange multiplier
+  ScalarVector g;   //!< Gap functor evaluated at the gap nodes
+  ScalarVector n_u; //!< Normal component of the solution at gap nodes
+  ScalarVector n_m; //!< Normal component of the lagrange multiplier at gap nodes
   
-  std::vector<std::set<int> > adjacencyPattern;
-  int quadratureOrder;
+  int          quadratureOrder;
+  EntityPointerVector boundary;
+  EntityPointerVector   active;
+  EntityPointerVector inactive;
+  EntityPointerVector   others;
   
+  const GapVertexMapper& gapMapper;
+  AIMapper&               aiMapper;
+
 public:
   SignoriniIASet (const TGV& _gv,  const TET& _a, const TFT& _f, const TTT& _p,
                   const TGT& _gap, int _quadratureOrder = 4);
   
   void setupMatrices ();
   void initialize ();
-  void assembleMain ();
-  void activateSet ();
+  void assemble ();
+  void determineActive ();
   void solve ();
   
   const CoordVector& solution() const { return u; }
@@ -100,62 +116,244 @@ public:
  * Implementation                                                             *
  ******************************************************************************/
 
-template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS>
-SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS>::SignoriniIASet (const TGV& _gv,
-                                                              const TET& _a,
-                                                              const TFT& _f,
-                                                              const TTT& _p,
-                                                              const TGT& _gap,
-                                                              int _quadratureOrder)
-: gv (_gv), a (_a), f (_f), p (_p), gap(_gap), quadratureOrder(_quadratureOrder)
+template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS, class TLM>
+SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS, TLM>::SignoriniIASet (const TGV& _gv,
+                                                                   const TET& _a,
+                                                                   const TFT& _f,
+                                                                   const TTT& _p,
+                                                                   const TGT& _gap,
+                                                                   int _quadratureOrder)
+: gv (_gv), a (_a), f (_f), p (_p), gap(_gap), quadratureOrder(_quadratureOrder),
+  gapMapper(_gv, _gap)
 {
   I = 0.0;
   for (int i=0; i < dim; ++i)
     I[i][i] = 1.0;
 }
 
-template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS>
-void SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS>::setupMatrices ()
+template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS, class TLM>
+void SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS, TLM>::setupMatrices ()
 {
-  VertexMapper       mapper (gv.grid());
-  GapVertexMapper gapMapper (gv, gap);
+  const int total = gv.size (dim);
+  const int ingap = gapMapper.size();  // *Should* be active.size() + inactive.size()
+
+  std::vector<std::set<int> > adjacencyPattern (total);
+  std::vector<std::set<int> > adjacencyPattern2 (ingap);
   
-  for (auto it = gv.template begin<dim>(); it != gv.template end<dim>(); ++it) {
-    cout << "Vertex #" << mapper.map (*it)
-         << " at " << it->geometry().center()
-         << " has new index " << gapMapper.map (*it) << "\n";
+    //For each element we traverse all its vertices and set them as adjacent
+  for (auto it = gv.template begin<0>(); it != gv.template end<0>(); ++it) {
+    const auto& ref = GenericReferenceElements<ctype, dim>::general (it->type ());
+    int vnum = ref.size (dim);
+    for (int i = 0; i < vnum; ++i)
+      for (int j = 0; j < vnum; ++j)
+        adjacencyPattern[aiMapper.map (*it, i, dim)] .
+            insert (aiMapper.map (*it, j, dim));
   }
   
+    // D is diagonal. FIXME: should I use another data type?
+  for (auto& x : active) {
+    int idx = gapMapper.map (x) - others.size();
+    adjacencyPattern2[idx].insert (idx);
+  }
+
+    //// Initialize default values
+
+  A.setSize (total, total);
+  A.setBuildMode (BlockMatrix::random);
+  
+  D.setSize (ingap, ingap);
+  D.setBuildMode (BlockMatrix::random);
+
+  b.resize (total, false);
+  u.resize (total, false);
+  
+  for (int i = 0; i < total; ++i)
+    A.setrowsize (i, adjacencyPattern[i].size ());
+  
+  for (int i = 0; i < ingap; ++i)
+      D.setrowsize (i, adjacencyPattern2[i].size ());
+  
+  A.endrowsizes ();
+  D.endrowsizes ();
+  
+  for (int i = 0; i < total; ++i)
+    for (const auto& it : adjacencyPattern[i])
+      A.addindex (i, it);
+  
+  for (int i = 0; i < ingap; ++i)
+    for (const auto& it : adjacencyPattern2[i])
+      D.addindex (i, it);
+
+  A.endindices ();
+  D.endindices ();
+  
+  A = 0.0;
+  D = 0.0;
+  b = 0.0;
+  u = 0.0;
+}
+
+template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS, class TLM>
+void SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS, TLM>::initialize ()
+{
   g.resize (gapMapper.size());
+  n_u.resize (gapMapper.size());
+  n_m.resize (gapMapper.size());
   
-}
-
-template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS>
-void SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS>::initialize ()
-{
-  setupMatrices();
-}
-
-template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS>
-void SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS>::assembleMain ()
-{
-  
-}
-
-template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS>
-void SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS>::activateSet ()
-{
   /*
-   ScalarVector ngap (gapCount);
-   ScalarVector nsol (gapCount);
-   ScalarVector nmul (gapCount);
-   AIFunctor activeset (ngap, nsol, nmul, c);
-   
+  for (const auto it = gv.template begin<0>(); it != gv.template end<0>(); ++it) {
+    for (auto is = gv.ibegin (*it) ; is != gv.iend (*it); ++is) {
+      if (is->boundary ()) {
+        boundary << *it;
+        break;
+      }
+    }
+  }
    */
+    //  Determine boundary entities (codim 1)
+  boundary.clear();
+  for (const auto it = gv.template begin<1>(); it != gv.template end<1>(); ++it)
+    if (it->boundary ())
+      boundary << *it;
+  cout << "The boundary contains " << boundary.size() << " entities.\n";
+  
+  bench().start ("Active set initialization");
+  determineActive();
+  bench().stop ("Active set initialization");
+  
+  bench().start ("Adjacency computation", false);
+  setupMatrices();
+  bench().stop ("Adjacency computation");
+
 }
 
-template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS>
-void SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS>::solve ()
+/*! Assemble the system matrix from the matrices A, D, N, T.
+
+ FIXME: I should simply reorder matrices D, N, T. Something like:
+ 
+   get the old iaMapper indices, and for each permutation old<->new, swap the
+   corresponding entries in the matrix.
+
+ */
+template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS, class TLM>
+void SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS, TLM>::assemble ()
+{
+    //// Recalculate matrix D.
+
+    // Recall that the integral is over the gap boundary, so we need not integrate
+    // the basis functions of nodes outside it.
+  
+  const auto& multBasis = TLM::instance();
+  const auto&     basis = TSS::instance();
+  const int      offset = others.size();
+  
+  for (const auto& e : boundary) {
+    EntityPointer in = e.inside();
+    const auto&  ref = GenericReferenceElements<ctype, dim>::general (in.type());
+    const int   vnum = ref.size (e.indexInInside (), 1, dim);
+    const auto& igeo = e.geometry ();
+    
+    if (gap.isSupported (igeo)) {
+      for (int i = 0 ; i < vnum; ++i) {
+        int subi = ref.subEntity (e.indexInInside (), 1, i, dim);
+        int   ii = aiMapper.map (e, subi, dim) - offset;  // FIXME: the offset is a hack
+        
+        for (auto& x : QuadratureRules<ctype, dim-1>::rule (e.type(), quadratureOrder)) {
+          auto& global = igeo.global (x.position ());
+          auto&  local = e.geometry().local (global);
+          D[ii][ii] += basis[subi].evaluateFunction (local) *
+                       multBasis[subi].evaluateFunction (local) *
+                       x.weight () *
+                       igeo.integrationElement (x.position ());
+        }
+      }
+    }
+  }
+  
+  /*
+  for (auto it = gv.template begin<dim>(); it != gv.template end<dim>(); ++it) {
+    GeometryType typ = it->type ();
+    const auto&  ref = GenericReferenceElements<ctype, dim>::general (typ);
+    const auto&  geo = it->geometry();
+    
+    for (auto is = gv.ibegin (*it) ; is != gv.iend (*it) ; ++is) {
+      if (is->boundary ()) {
+        const int  ivnum = ref.size (is->indexInInside (), 1, dim);
+        const auto& igeo = is->geometry ();
+        const auto& ityp = is->type ();
+        
+        if (gap.isSupported (igeo)) {
+          for (int i = 0 ; i < ivnum; ++i) {
+            int subi = ref.subEntity (is->indexInInside (), 1, i, dim);
+            int   ii = aiMapper.map (*it, subi, dim);
+            
+            for (auto& x : QuadratureRules<ctype, dim-1>::rule (ityp, quadratureOrder)) {
+              auto& global = igeo.global (x.position ());
+              auto&  local = it->geometry().local (global);
+              D[ii][ii] += basis[subi].evaluateFunction (local) *
+              multBasis[subi].evaluateFunction (local) *
+              x.weight () *
+              igeo.integrationElement (x.position ());
+            }
+          }
+        }
+      }
+    }
+  }
+  */
+  
+  
+    /// Recalculate matrix N
+  const int K = active.size();
+  N.setSize (K, K);
+  N.setBuildMode (BlockMatrix::random);
+
+  for (const auto& e : boundary) {
+    EntityPointer in = e.inside();
+    const auto&  ref = GenericReferenceElements<ctype, dim>::general (in.type());
+    const int   vnum = ref.size (e.indexInInside (), 1, dim);
+    const auto& igeo = e.geometry ();
+    coord_t n = e.unitOuterNormal ();
+    for (int i = 0 ; i < vnum; ++i) {
+      int subi = ref.subEntity (e.indexInInside (), 1, i, dim);
+      int   ii = aiMapper.map (e, subi, dim) - offset;  // FIXME: the offset is a hack
+    }
+  }
+}
+
+template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS, class TLM>
+void SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS, TLM>::determineActive ()
+{
+  AIFunctor contact (g, n_u, n_m);  // 0 initial values *should* imply empty support
+  
+  active.clear();
+  inactive.clear();
+  others.clear();  // FIXME: no need to recalculate this!
+  
+  for (auto it = gv.template begin<dim>(); it != gv.template end<dim>(); ++it) {
+    if (gap.isSupported (it->geometry())) {
+      if (contact.isSupported (it->geometry()))
+        active << *it;
+      else
+        inactive << *it;
+    } else {
+      others << *it;
+    }
+  }
+  
+  aiMapper.update (active, inactive, others);
+  
+  cout << "Others:";
+  for (auto& x : others)   cout << " " << aiMapper.map(x);
+  cout << "\nInactive:";
+  for (auto& x : inactive) cout << " " << aiMapper.map(x);
+  cout << "\nActive:";
+  for (auto& x : active)   cout << " " << aiMapper.map(x);
+  cout << "\n";
+}
+
+template<class TGV, class TET, class TFT, class TTT, class TGT, class TSS, class TLM>
+void SignoriniIASet<TGV, TET, TFT, TTT, TGT, TSS, TLM>::solve ()
 {
   
 }
