b1b7e6c89dd41829fdc8ea10449698ec6479a100
Miguel de Benito Delgado <nonick@8027.org>
2013-04-28 10:28:49 +0200

    Using P1ShapeFunctionSet for the simplicial grids of the cylinders.

--- signorini/main.cpp
+++ signorini/main.cpp
@@ -27,7 +27,7 @@ pdo.SetPoints(newPoints)
 
   //// Dune includes
 
-#include "config.h"                    // file constructed by ./configure script
+#include "config.h"             // (initially) constructed by ./configure script
 #include <dune/grid/sgrid.hh>
 #include <dune/common/mpihelper.hh>
 #include <dune/common/exceptions.hh>
@@ -49,11 +49,6 @@ pdo.SetPoints(newPoints)
 #include "twobodies.hpp"
 #include "physicalgroupdescriptors.hpp"
 
-  //// stdlib includes
-
-//#include <string>
-//using std::string;
-
 int main (int argc, char** argv)
 {
   const int          dim = 3;
@@ -62,35 +57,37 @@ int main (int argc, char** argv)
   const double        E2 = 8.0e9;
   const double        nu = 0.3;
   const double       nu2 = 0.3;
-  
-  //const double         E = 5.0e9;       // Young's modulus (in Pa) [FV05, p.35]
-  //const double       eps = 1.0e-5 / E;  // See [KO88, p.140]
-  //const double       eps = 1.0e-14 / E;  // See [KO88, p.140]
-  //const double tolerance = 1.0e-5;      // For the iterative penalty method
-  //const int     maxsteps = 10;
-
+/*
+  const double         E = 5.0e9;       // Young's modulus (in Pa) [FV05, p.35]
+  const double       eps = 1.0e-5 / E;  // See [KO88, p.140]
+  const double       eps = 1.0e-14 / E;  // See [KO88, p.140]
+  const double tolerance = 1.0e-5;      // For the iterative penalty method
+  const int     maxsteps = 10;
+*/
     //// Grid setup
 
 //  typedef SGrid<dim, dim>          grid_t;
-  typedef ALUCubeGrid<dim, dim>    grid_t;
+  typedef ALUSimplexGrid<dim, dim> grid_t;
   typedef GridFactory<grid_t>   factory_t;
   typedef grid_t::ctype             ctype;
   typedef FieldVector<ctype, dim> coord_t;
   typedef grid_t::LeafGridView         GV;
 
+  /*
   FieldVector<int, dim> N(1);
   coord_t       origin (0.0);
   coord_t     topright (1.0);
-  /*
-  grid_t gridSlave (N, origin, topright);
-  gridSlave.globalRefine (3);
+  grid_t*           grids[2];
+
+  grids[SLAVE] = new grid_t (N, origin, topright);
+  grids[SLAVE]->globalRefine (4);
   
     // Careful changing this! Check the supports of the boundary functors!!
   origin[1] = -1;
   topright[1] = 0;
-  grid_t gridMaster (N, origin, topright);
-  gridMaster.globalRefine (3);
-*/
+  grids[MASTER] = new grid_t (N, origin, topright);
+  grids[MASTER]->globalRefine (4);
+   */
   
   string path ("/Users/miguel/Devel/Signorini/meshes/");
   factory_t factories[2];
@@ -111,7 +108,7 @@ int main (int argc, char** argv)
   grids[SLAVE]  = factories[SLAVE].createGrid();
   
     //// Setup contact surfaces
-  
+
   typedef typename grid_t::LevelGridView LevelGV;
   typedef Codim1Extractor<LevelGV> SurfaceExtractor;
   typedef PSurfaceMerge<dim-1, dim, double> SurfaceMergeImpl;
@@ -147,10 +144,15 @@ int main (int argc, char** argv)
   typedef DirichletFunctor<ctype, dim>  Dirichlet;
   typedef HookeTensor<ctype, dim>          HookeT;
 
-  typedef MLinearShapeFunction<ctype, dim>             ShapeF;
-  typedef Q1ShapeFunctionSet<ctype, dim, ShapeF>     ShapeSet;
-  typedef LagrangeSpaceShapeFunction<ctype, dim>     LSShapeF;
-  typedef Q1ShapeFunctionSet<ctype, dim, LSShapeF> LSShapeSet;
+//  typedef BetterLinearShapeFunction<ctype, dim, 1, 0> ShapeF;
+//  typedef MLinearShapeFunction<ctype, dim>         ShapeF;
+//  typedef Q1ShapeFunctionSet<ctype, dim, ShapeF> ShapeSet;
+//  typedef BetterLinearShapeFunction<ctype, dim, 3, -1> LSShapeF;
+//  typedef LagrangeSpaceShapeFunction<ctype, dim>     LSShapeF;
+//  typedef Q1ShapeFunctionSet<ctype, dim, LSShapeF> LSShapeSet;
+
+  typedef P1ShapeFunctionSet<ctype, dim, 1, 0> ShapeSet;
+  typedef P1ShapeFunctionSet<ctype, dim, 3, -1> LSShapeSet;
   
   typedef SignoriniFEPenalty<GV, HookeT, VolumeF, BoundaryF, Gap, Dirichlet, ShapeSet>
           PMSolver;
@@ -162,22 +164,20 @@ int main (int argc, char** argv)
     //Should be:
     //SignoriniIASet<GV, ProblemData, ShapeSet> IASolver;
   
-//  testShapes<ctype, dim, ShapeSet>("/tmp/basis");
-//  testShapes<ctype, dim, LSShapeSet>("/tmp/multbasis");
+//  testShapes<ctype, dim, ShapeSet> ("/tmp/basis");
+//  testShapes<ctype, dim, LSShapeSet> ("/tmp/multbasis");
 //  test_basisOfPlaneNormalTo<ctype, dim>();
 //  exit(1);
 
   HookeT    a (E, nu);
   HookeT    a2 (E2, nu2);
+    
   VolumeF   f (coord3 (0.0, 0.0, 0.0));
   VolumeF   f2 (coord3 (0.0, 0.0, 0.0));
   Dirichlet d (coord3 (0.0, -0.07, 0.0));
   Dirichlet d2 (coord3 (0.0, 0.0, 0.0));
   BoundaryF p (coord3 (-3e6, -7e6, 0.0));
   BoundaryF p2 (coord3 (3e6, -4e6, 0.0));
-//  Gap       g (0.0, 0.0);
-//  Gap       g2 (0.0, 0.0);
-  Gap g (0.0, 0.05);
 
 //  VolumeF   f (coord2 (0.0, 0.0));
 //  VolumeF   f2 (coord2 (0.0, 0.0));
@@ -185,9 +185,10 @@ int main (int argc, char** argv)
 //  Dirichlet d2 (coord2 (0.0, 0.0));
 //  BoundaryF p (coord2 (-3e6, -7e6));
 //  BoundaryF p2 (coord2 (3e6, -4e6));
-//    //  Gap       g (0.0, 0.0);
-//    //  Gap       g2 (0.0, 0.0);
-//  Gap g (0.0, 0.05);
+
+    //  Gap       g (0.0, 0.0);
+    //  Gap       g2 (0.0, 0.0);
+  Gap g (0.0, 0.05);
   
   
     //PMSolver  fem (gridSlave.leafView(), a, f, p, g, d, eps);
